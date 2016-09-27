cb3e6fc3b17aba329350617c29021ed64f3c7916
chaduhduh <chadd.myers3@gmail.com>
2016-09-26 19:40:24 -0700

    add fields to level

--- Level.py
+++ Level.py
@@ -4,16 +4,51 @@
     built from this accordingly.
 """
 
+
 class Level():
     """  """
     
     _name = "new-game"
+    _pieces = []
+    _solutions = []
+    _board_structure = {};
+
+
+    # name
 
     def setName(self, name):
         self._name = name;
 
     def getName(self):
         return self._name or ""
+
+
+    # pieces
+
+    def setPieces(self, pieces):
+        self._pieces = pieces;
+
+    def getPieces(self):
+        return self._pieces or []
+
+
+    # solutions
+
+    def setSolutions(self, solutions):
+        self._solutions = solutions;
+
+    def getSolutions(self):
+        return self._solutions or []
+
+
+    # board structure
+
+    def setBoardStructure(self, structure):
+        self._board_structure = structure;
+
+    def getBoarStructure(self):
+        return self._board_structure or []
+
 
 
 class All_Levels():
@@ -24,6 +59,24 @@ class All_Levels():
     # Level One
     level_one = Level()
     level_one.setName("level_one")
+    level_one.setPieces(["start","print","'game'","end"]);
+    level_one.setSolutions([["start","print","'game'","end"]]);
+    level_one.setBoardStructure({
+        "main" : {
+            "row" : {
+                "pieces" : ["start"]
+            }
+            "row" : {
+                "pieces" : ["print", "'game'"]
+                "row" : {
+                    "pieces" : []       # this is how nested rows are defined
+                }
+            }
+            "row" : {
+                "pieces" : ["end"]
+            }
+        }
+    });
     levels.append(level_one)
 
 
