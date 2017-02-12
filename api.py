b59a2076aab96bbff8a9022f74fc11cf2bcf1c88
chaduhduh <chadd.myers3@gmail.com>
2017-02-12 13:41:37 -0700

    fixes create user to use the correct name

--- api.py
+++ api.py
@@ -86,10 +86,11 @@ class ProgrameApi(remote.Service):
 
         user = endpoints.get_current_user()
         user_name = user.email() if user else 'Anonymous'
+        # TODO: this should use the oauth info
         if user_name is None or user_name is 'Anonymous':
             raise endpoints.UnauthorizedException(
                     'Log in to create a user')
-        if User.query(User.name == user_name).get():
+        if User.query(User.name == request.user_name).get():
             raise endpoints.ConflictException(
                     'A User with that name already exists!')
         user = User(name=request.user_name, email=request.email)
